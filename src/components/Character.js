0c7fa0f2574825c37642c96d6d3cdf14b9cd3c43
Bobby Lane <bobbylane@Bobbys-MacBook-Pro.local>
2020-12-11 13:30:44 -0500

    Sprint challenge React take 1

--- src/components/Character.js
+++ src/components/Character.js
@@ -1,1 +1,16 @@
 // Write your Character component here
+import React from "react";
+import Index from "./index";
+
+export default function Character(props) {
+
+    const {name, id} = props;
+
+    return (
+        <div className = "character-character container">
+            <div className = "character-info">
+                <h1> {character.name} {character.id} </h1>
+            </div>
+        </div>
+        
+    )
